cdd5b4ed33d41e1862543b022a4274444f5fff13
b7018132 <b7018132@my.shu.ac.uk>
2019-04-04 14:49:47 +0100

    Completed login for doctor

--- appointmentSystem/grails-app/controllers/com/ebee/DoctorController.groovy
+++ appointmentSystem/grails-app/controllers/com/ebee/DoctorController.groovy
@@ -3,4 +3,35 @@ package com.ebee
 class DoctorController {
 
 	def scaffold=Doctor
+
+		def login(){
+
+		}
+	
+		def validate(){
+		
+		def user = Doctor.findByDoctorEmail(params.email)
+
+		if ( user && user.password == params.password){
+
+			session.user = user
+			render view:'home'
+		}
+
+		else{
+
+		flash.message = "Invalid username and password."
+
+		render view:'login'
+		
+		}
+		}
+		def logout = {
+		
+		session.user = null
+
+		redirect(uri:'/')
+
+		}
+
 }
